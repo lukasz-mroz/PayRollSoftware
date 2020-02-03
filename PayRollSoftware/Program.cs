b2fa8e3c9a26761a8c03117576c8784fdfb6b051
mrozikpl <39560906+mrozikpl@users.noreply.github.com>
2020-02-03 17:13:35 +0100

    Add project files.

---
+++ PayRollSoftware/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace PayRollSoftware
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+  // contains information about each staff in the company
+  class Staff
+  {
+    private float hourlyRate;
+    private int hWorked;
+
+    public float TotalPay { get; protected set; }
+    public float BasicPay { get; private set; }
+    public string NameOfStaff { get; private set; }
+    public int HoursWorked { get; }
+
+    public Staff(string name,float rate)
+    {
+      NameOfStaff = name;
+      hourlyRate = rate;
+    }
+
+    public virtual void CalculatePay()
+    {
+      Console.WriteLine("Calculating Pay...");
+      var sum = hWorked * hourlyRate;
+    }
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  class Manager 
+  {
+
+  }
+  class Admin
+  {
+
+  }
+  class FileReader
+  {
+
+  }
+  class PaySlip
+  {
+
+  }
+
+}
